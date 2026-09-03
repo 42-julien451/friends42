2f102aad12b6aae87c055e6cc39c823d73439154
julien451 <julien451@42paris.fr>
2026-09-03 17:15:36 +0200

    added a print for debugging

--- updater.py
+++ updater.py
@@ -2,20 +2,17 @@ import requests
 import time
 import config
 
-campuses = config.campuses_to_update
-
 if __name__ == '__main__':
 	while True:
-		for campus in campuses:
-			print(f'updating 42Paris campus...')
-			try:
-				print("Updating locations")
-				req = requests.get(f'http://127.0.0.1:8080/locations/{config.update_key}')
-				print("Updating DEAD PC...")
-				req = requests.get(f'http://127.0.0.1:8080/modules/{config.update_key}')
-				print(req.status_code)
-			except requests.exceptions.RequestException:
-				print('failed req')
-			time.sleep(5)
+		print(f'updating 42Paris campus...')
+		try:
+			print("Updating locations")
+			req1 = requests.get(f'http://127.0.0.1:8080/locations/{config.update_key}')
+			print(req1.status_code)
+			print("Updating DEAD PC...")
+			req2 = requests.get(f'http://127.0.0.1:8080/modules/{config.update_key}')
+			print(req2.status_code)
+		except requests.exceptions.RequestException as e:
+			print(f'failed req:\n{e}')
 		print('waiting 45s')
 		time.sleep(45)
