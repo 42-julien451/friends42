4f2fe01e04f77f3d42dd15002f4ea0736c236d63
julien451 <julien451@42paris.fr>
2026-09-03 17:09:41 +0200

    Added ratatouille support, remove old intrav2 creds

--- globals.py
+++ globals.py
@@ -1,6 +1,12 @@
 import redis
-from api42 import Api
 import config
+try:
+    import RatatouilleClient
+    ratatouille = RatatouilleClient.RatatouilleClient(config.ratatouille_client, config.ratatouille_secret, realm="next");
+except ImportError:
+    ratatouille = None;
+from api42 import Api
+
 
 r = redis.Redis(host=config.redis_host, port=config.redis_port, db=0)
-api = Api(config.key, config.secret)
+api = Api()
