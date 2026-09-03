29fb1cd665ae3a2018790edd2609d60a035e5c3e
julien451 <julien451@42paris.fr>
2026-09-03 14:20:15 +0200

    Removed multicampus support + added node exporter support

--- updater.py
+++ updater.py
@@ -7,9 +7,12 @@ campuses = config.campuses_to_update
 if __name__ == '__main__':
 	while True:
 		for campus in campuses:
-			print(f'updating campus {campus}...')
+			print(f'updating 42Paris campus...')
 			try:
-				req = requests.get(f'http://127.0.0.1:8000/locations/{config.update_key}/{campus}')
+				print("Updating locations")
+				req = requests.get(f'http://127.0.0.1:8080/locations/{config.update_key}')
+				print("Updating DEAD PC...")
+				req = requests.get(f'http://127.0.0.1:8080/modules/{config.update_key}')
 				print(req.status_code)
 			except requests.exceptions.RequestException:
 				print('failed req')
